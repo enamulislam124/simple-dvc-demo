0285841c580085b22c1ace9a1b9c0c68e8737bc9
enamulislam124 <islamenamul124@gmail.com>
2022-10-24 18:44:06 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name="src",
+    version="0.0.1",
+    description="its a wine Q package",
+    author="enamulislam124",
+    packages=find_packages(),
+    license="MIT"
+)
+
